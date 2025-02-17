b955596e7fb6f1cffe88ce7a6cafac4b95c95470
unknown <toobabaqai1@gmail.com>
2025-02-17 20:28:19 +0500

    commit

---
+++ mad.js
@@ -0,0 +1,18 @@
+let objArr=[
+    {
+        name:'Tooba',
+        age: 19
+    },
+    {
+        name: 'Faareha',
+        age: 20
+    },
+    {
+        name: 'Maham',
+        age: 20
+    }
+];
+function print_data(data){
+    console.log(data);
+}
+ objArr.map(print_data);
